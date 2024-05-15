261d7508b3c1c0321fc93bd03ed49d56432a040f
Lino Khan <Lino Khan>
2024-05-15 13:35:33 +0200

    Style update

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -2,6 +2,9 @@ import React from "react";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '@fortawesome/fontawesome-free/css/all.css';
 import Accordion from "../../components/specific/Accordion";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import Image from '../../assets/images/Image.jpg';
+import '../../pages/Home/Home.module.scss';
 
 
 const Home = () => {
@@ -20,17 +23,23 @@ const Home = () => {
                 <div className="hero-text">
 
                   <h1 className="display-1">Get a beautiful website that drives revenue</h1>
-                  <p className="display-6">Results-driven web design, development, SEO, branding, copywriting & digital strategy.</p>
+                  <p>Results-driven web design, development, SEO, branding, copywriting & digital strategy.</p>
                   <span>
-                    <button> Get a proposal</button>
-                    <button> View services</button>
+                    <button className="btn-primary"> Get a proposal</button>
+                    <button className="btn-secondary"> View services</button>
                   </span>
 
                 </div>
                 <div className="hero-testimonial">
-                  <span>Stars rating</span>
+                  <span>
+                  <FontAwesomeIcon icon="fa-regular fa-star" />
+                  <FontAwesomeIcon icon="fa-regular fa-star" />
+                  <FontAwesomeIcon icon="fa-regular fa-star" />
+                  <FontAwesomeIcon icon="fa-regular fa-star" />
+                  <FontAwesomeIcon icon="fa-regular fa-star" />
+                  </span>
                   <p>Our new site has a 60% higher conversion rate than our old one</p>
-                  <span> <h2>John Smith</h2></span>
+                  <span> <h6>John Smith</h6></span>
                 </div>
 
               </div>
@@ -52,18 +61,18 @@ const Home = () => {
               <div className="col">
                 <span>Rating stars</span>
                 <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Deserunt ipsum, sit in officiis harum, quidem et accusantium esse culpa debitis nihil eos quibusdam sunt maiores. Debitis rem ipsum veritatis sapiente!</p>
-                <span> <h2>John Doe</h2></span>
+                <span> <h6>John Doe</h6></span>
               </div>
               <div className="col">
                 <span>Rating stars</span>
                 <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Deserunt ipsum, sit in officiis harum, quidem et accusantium esse culpa debitis nihil eos quibusdam sunt maiores. Debitis rem ipsum veritatis sapiente!</p>
-                <span> <h2>John Doe</h2></span>
+                <span> <h6>John Doe</h6></span>
 
               </div>
               <div className="col">
                 <span>Rating stars</span>
                 <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Deserunt ipsum, sit in officiis harum, quidem et accusantium esse culpa debitis nihil eos quibusdam sunt maiores. Debitis rem ipsum veritatis sapiente!</p>
-                <span> <h2>John Doe</h2></span>
+                <span> <h6>John Doe</h6></span>
 
               </div>
             </div>
@@ -78,11 +87,11 @@ const Home = () => {
 
             <div className="row">
               <div className="col-lg-6">
-                <img src="" alt="" />
+                <img className="img-fluid" src={Image} alt="Image" />
 
               </div>
               <div className="col-lg-6">
-                <h1 className="display-4">Everything your site needs for success</h1>
+                <h1>Everything your site needs for success</h1>
                 <ul>
                   <li>Web design and development</li>
                   <li>Search Engine Optimization SEOs</li>
@@ -103,29 +112,32 @@ const Home = () => {
 
             <div className="row">
               <div className="col-lg-7">
-                <h1 className="display-4">Over the past 5 years, we have delivered measurable results for 113+ businesses.</h1>
+                <h1>Over the past 5 years, we have delivered measurable results for 113+ businesses.</h1>
               </div>
             </div>
             <div className="row">
               <div className="col">
-                <h1>26.3</h1>
-                <h2>Lorem ipsum dolor sit amet consectetur adipisicing elit. </h2>
+                <h2>26.3</h2>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. </p>
 
               </div>
               <div className="col">
-                <h1>26.3</h1>
-                <h2>Lorem ipsum dolor sit amet consectetur adipisicing elit. </h2>
+                <h2>26.3</h2>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. </p>
+
+
+
+              </div>
+              <div className="col">
+                <h2>26.3</h2>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. </p>
 
 
               </div>
               <div className="col">
-                <h1>26.3</h1>
-                <h2>Lorem ipsum dolor sit amet consectetur adipisicing elit. </h2>
+                <h2>26.3</h2>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. </p>
 
-              </div>
-              <div className="col">
-                <h1>26.3</h1>
-                <h2>Lorem ipsum dolor sit amet consectetur adipisicing elit. </h2>
 
               </div>
 
@@ -142,34 +154,34 @@ const Home = () => {
           <div className="section">
 
             <div className="title">
-              <h1 className="display-4">Helpful resources</h1>
+              <h1>Helpful resources</h1>
             </div>
             <div className="row">
               <div className="col">
                 <div className="img">
-                  <img src="" alt="" />
+                <img className="img-fluid" src={Image} alt="Image" />
                 </div>
                 <div className="blog-preview">
-                  <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. </h1>
+                  <h2>Lorem ipsum dolor sit amet consectetur adipisicing elit. </h2>
                   <a href="">Read</a>
                 </div>
 
               </div>
               <div className="col">
                 <div className="img">
-                  <img src="" alt="" />
+                <img className="img-fluid" src={Image} alt="Image" />
                 </div>
                 <div className="blog-preview">
-                  <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. </h1>
+                <h2>Lorem ipsum dolor sit amet consectetur adipisicing elit. </h2>
                   <a href="">Read</a>
                 </div>
               </div>
               <div className="col">
                 <div className="img">
-                  <img src="" alt="" />
+                <img className="img-fluid" src={Image} alt="Image" />
                 </div>
                 <div className="blog-preview">
-                  <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. </h1>
+                <h2>Lorem ipsum dolor sit amet consectetur adipisicing elit. </h2>
                   <a href="">Read</a>
                 </div>
               </div>
@@ -185,7 +197,7 @@ const Home = () => {
 
             <div className="row">
               <div className="title">
-                <h1 className="display-4">Frequently asked questions</h1>
+                <h1>Frequently asked questions</h1>
               </div>
               <Accordion />
             </div>
